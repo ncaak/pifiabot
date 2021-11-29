12174c413bfa0529d623e1de276367b9866068f1
Isaac Jimeno <isaacjim.dev@gmail.com>
2021-11-29 19:26:41 +0100

    Refactored main script to resemble on new package

--- main.go
+++ main.go
@@ -1,46 +1,27 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ncaak/pifiabot/server"
 )
 
-func getCertificates() []tls.Certificate {
-	var cert, err = tls.LoadX509KeyPair("localhost.crt", "localhost.key")
-	if err != nil {
-		log.Fatalf("ERROR :: Certicate could not be retrieved : %v", err.Error())
-	}
-	return []tls.Certificate{cert}
-}
-
-func getMuxHandler() *http.ServeMux {
-	var mux = http.NewServeMux()
-	// API Methods
-	mux.HandleFunc("/v1/bot-api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("que entro")
-	})
-
-	return mux
-}
-
-func getServer() *http.Server {
-	return &http.Server{
-		Addr:    ":8443",
-		Handler: getMuxHandler(),
-		TLSConfig: &tls.Config{
-			Certificates: getCertificates(),
-		},
-	}
+func webhook(w http.ResponseWriter, r *http.Request) {
+	log.Println("INFO :: Request received")
+	fmt.Fprintf(w, "OK")
 }
 
 func main() {
 
 	fmt.Println("INFO :: Starting the server...")
 
-	log.Fatal(
-		getServer().ListenAndServeTLS("", ""),
-	)
+	var service = server.Build()
 
+	service.AddRoute("/v1/bot-api", webhook)
+
+	log.Fatal(
+		service.Listen("8443"),
+	)
 }
