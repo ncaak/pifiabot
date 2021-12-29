a7c46dc29f13f7f5f759a4aef36ff29347381146
Isaac Jimeno <isaacjim.dev@gmail.com>
2021-12-29 21:18:32 +0100

    Added new refactored functions client package

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"time"
@@ -32,6 +33,25 @@ func (c client) sendRequest(r *http.Request) error {
 	return err
 }
 
+func send(url string, body []byte) {
+	var req, errReq = http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if errReq != nil {
+		log.Println("ERROR :: Creating Request Object : " + errReq.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	var client = &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	var _, errDo = client.Do(req)
+	if errDo != nil {
+		log.Println("ERROR :: Sending HTTP request : " + errReq.Error())
+	}
+}
+
 func (c client) SendReply(method string, body models.Output) {
 	var req, err = requests.Reply(c.Url+method, body)
 	if err != nil {
@@ -43,5 +63,14 @@ func (c client) SendReply(method string, body models.Output) {
 	if errRequest != nil {
 		log.Println("ERROR :: Sending Reply request")
 		return
+	}
+}
+
+func (c client) SendMessage(data models.Output) {
+	var endpoint = c.Url + "sendMessage"
+	var body = requests.GetReplyBytes(data)
+
+	if len(body) > 0 {
+		send(endpoint, body)
 	}
 }
