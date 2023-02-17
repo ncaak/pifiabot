da0ae0e0cb35929f40247fb9081f0ff95d92d766
Isaac Jimeno <isaacjim.dev@gmail.com>
2023-02-17 01:16:39 +0100

    Refactor actions factory with error handling

--- actions/factory.go
+++ actions/factory.go
@@ -1,19 +1,24 @@
 package actions
 
-import (
-	"strings"
-)
+import "regexp"
 
 const MAX_COMMAND_LENGTH = 50
+const MSG_UNKNOWN_ACTION = "unknown_action"
+const ERR_UNKNOWN = "unknown_error"
 
-type Action interface {
+type ActionInterface interface {
 	Resolve() (string, error)
 }
 
-func Factory(command string) Action {
+func Factory(command string) ActionInterface {
+	if len(command) > MAX_COMMAND_LENGTH {
+		return NewErrorAction("notation_max_length")
+	}
+
 	switch {
 	// TODO: Define commands as they are coded
-	case strings.HasPrefix(command, "/t"):
+	// case strings.HasPrefix(command, "/t"):
+	case regexp.MustCompile(`^/(t|tira)(\s|$)`).MatchString(command):
 		return RollAction{command: command}
 	default:
 		return UnknownAction{}
